c2a10e0d7f61212516c0c5eedd4b6275cf35c99b
Thomas Phillips <tphillips2020@gmail.com>
2024-11-14 00:08:19 +1030

    Changed lsp-config to plugin-config to act as a universal plugin configurator

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ require("keymaps")
 
 require("lazy-bootstrap")
 
-require("lsp-config")
+require("plugin-config")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
